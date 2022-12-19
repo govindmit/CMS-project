b43fcfcfc4e8e74f51a0d62e3820cefd20f5c701
mangoitsols <govind@mangoitsolutions.com>
2022-12-19 17:38:24 +0530

    baseurl changes

--- Service/http-common.js
+++ Service/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL:"http://192.168.168.29:8080",
-    baseURL:"https://curious-veil-frog.cyclic.app/",
+    baseURL:"http://192.168.168.29:8080",
+    // baseURL:"https://curious-veil-frog.cyclic.app/",
 
     headers: {
         "Content-type": "application/json"
